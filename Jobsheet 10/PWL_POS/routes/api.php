312a5b25eb38eb9dc39f0248c4fc963c708248b6
Kartika Tri Juliana <kartikatjuliana@gmail.com>
2025-04-23 20:47:30 +0700

    prak 2

--- Jobsheet 10/PWL_POS/routes/api.php
+++ Jobsheet 10/PWL_POS/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,9 @@ use App\Http\Controllers\Api\RegisterController;
 |
 */
 
-Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/register', RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
--- Jobsheet 10/PWL_POS/routes/web.php
+++ Jobsheet 10/PWL_POS/routes/web.php
@@ -163,9 +163,9 @@ Route::group(['prefix' => 'stok'], function () {
 
 Route::pattern('id', '[0-9]+'); // artinya ketika ada parameter {id}, maka harus berupa angka
 
-Route::get('login', [AuthController::class, 'login'])->name('login');
-Route::post('login', [AuthController::class, 'postlogin']);
-Route::get('logout', [AuthController::class, 'logout'])->middleware('logout');
+//Route::get('login', [AuthController::class, 'login'])->name('login');
+//Route::post('login', [AuthController::class, 'postlogin']);
+//Route::get('logout', [AuthController::class, 'logout'])->middleware('logout');
 
 Route::middleware(['auth'])->group(function () { // artinya semua route di dalam group ini harus login dulu
     Route::get('/', [WelcomeController::class, 'index']);
